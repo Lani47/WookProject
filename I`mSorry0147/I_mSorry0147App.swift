20f406db88ad0ef6749f341d4714c5d6f110ed05
LaniLaniLani47 <21cm0147@jec.ac.jp>
2022-07-04 20:57:31 +0900

    Initial Commit

---
+++ I`mSorry0147/I_mSorry0147App.swift
@@ -0,0 +1,17 @@
+//
+//  I_mSorry0147App.swift
+//  I`mSorry0147
+//
+//  Created by cmStudent on 2022/07/04.
+//
+
+import SwiftUI
+
+@main
+struct I_mSorry0147App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
